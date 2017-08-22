db1028dfdc41d5f034a437a7f2ed00bb0260152d
Grigory Romodanovskiy <grigory@weme.sh>
2017-08-22 13:47:53 -0400

    502 instead of 500 when service timeouts

--- src/main/scala/org/red/cerberus/http/Middleware.scala
+++ src/main/scala/org/red/cerberus/http/Middleware.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 import com.typesafe.scalalogging.LazyLogging
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+import com.twitter.finagle._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.red.iris._
@@ -36,6 +37,9 @@ trait Middleware extends LazyLogging with FailFastCirceSupport {
         logger.error("CCP Exception, " +
           s"reason=$reason")
         complete(HttpResponse(StatusCodes.ServiceUnavailable, entity = ErrorResponse("Error: CCP exception")))
+      case exc: IndividualRequestTimeoutException =>
+        logger.error(s"One of the thrift services timed out thriftServiceName=${exc.serviceName}", exc)
+        complete(HttpResponse(StatusCodes.BadGateway, entity = ErrorResponse("Service is not available")))
       case exc@AccessRestrictedException(reason) =>
         logger.info("Banned user attempted to login")
         complete(HttpResponse(StatusCodes.UnavailableForLegalReasons, entity = ErrorResponse(reason)))
